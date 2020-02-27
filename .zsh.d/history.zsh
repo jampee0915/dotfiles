4c763c3fc9cccc83a5d58d93fc4381ca308a4895
jumpei.toyoda <jampee@jumpeitoyodanoMacBook-Pro.local>
2020-02-28 01:36:19 +0900

    first commit

---
+++ .zsh.d/history.zsh
@@ -0,0 +1,35 @@
+# 履歴ファイルの保存先
+export HISFILE=${HOME}/.zsh_history
+
+# メモリに保存される履歴の件数
+export HISTSIZE=1000
+
+# 履歴ファイルに保存される履歴の件数
+export SAVEHIST=100000
+
+# 重複を記録しない
+setopt hist_ignore_dups
+
+# 開始と終了を記録
+setopt EXTENDED_HISTORY
+
+# ヒストリファイルに保存するとき、すでに重複したコマンドがあったら古い方を削除する
+setopt hist_save_nodups
+
+# スペースから始まるコマンド行はヒストリに残さない
+setopt hist_ignore_space
+
+# ヒストリに保存するときに余分なスペースを削除する
+setopt hist_reduce_blanks
+
+# 補完候補が複数あるときに自動的に一覧表示する
+setopt auto_menu
+
+# pecoでhistory検索
+function peco-select-history() {
+  BUFFER=$(\history -n -r 1 | peco --query "$LBUFFER")
+  CURSOR=$#BUFFER
+  zle clear-screen
+}
+zle -N peco-select-history
+bindkey '^r' peco-select-history
---
+++ .zshrc
@@ -0,0 +1,9 @@
+ZSHOME="${HOME}/.zsh.d"
+if [ -d $ZSHHOME -a -r $ZSHHOME -a \
+        ¦-x $ZSHHOME ]; then
+    for i in $ZSHHOME/*; do
+        [[ ${i##*/} = *.zsh ]] &&
+            [ \( -f $i -o -h $i \) -a -r $i ] && . $i
+    done
+fi
+
