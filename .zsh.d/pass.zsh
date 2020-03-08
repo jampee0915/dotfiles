d9087926000c2361425a862eed1c3c677f7e70de
jumpei.toyoda <jampee@jumpeitoyodanoMacBook-Pro.local>
2020-03-08 14:31:10 +0900

    add vim clipboard setting

---
+++ .zsh.d/pass.zsh
@@ -0,0 +1,18 @@
+# golang
+export GOPATH=$HOME/go
+export GOROOT="$(brew --prefix golang)/libexec"
+export PATH=$PATH:$HOME/go/bin
+
+# java
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/amazon-corretto-8.jdk/Contents/Home
+export PATH="$JAVA_HOME/bin:$PATH"
+
+# pyenv
+export PYENV_ROOT=/usr/local/var/pyenv
+
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
+
+# nodenv
+eval "$(nodenv init -)"
