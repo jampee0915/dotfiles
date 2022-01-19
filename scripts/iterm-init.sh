7208c2fdf09bdfe36d5a920f376992ea980d0a63
jampee <jun19900915@gmail.com>
2022-01-19 22:24:55 +0900

    fix curl path

--- scripts/iterm-init.sh
+++ scripts/iterm-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install fonts
-curl -fLo ~/Library/Fonts https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+curl -fLo ~/Library/Fonts/MesloLGS%20NF%20Regular.ttf https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
