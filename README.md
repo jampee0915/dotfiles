64640c0097a7e67cfaf9872e6cc19eb1841d9c58
jampee <jun19900915@gmail.com>
2022-01-19 19:20:50 +0900

    fix bug

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Install
 ```
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/jampee0915/dotfiles/master/install.sh)
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/jampee0915/dotfiles/master/install.sh)"
 ```
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ has() {
     type "$1" > /dev/null 2>&1
 }
 
-if if [ ! -d ${DOT_DIR} ]; then
+if [ ! -d ${DOT_DIR} ]; then
     if has "git"; then
         git clone https://github.com/jampee0915/dotfiles ${DOT_DIR}
     elif has "curl" || has "wget"; then
