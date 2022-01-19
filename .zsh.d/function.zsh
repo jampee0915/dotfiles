b6e07b79c48e087ac939d0157cd5430beb34c937
jampee <jun19900915@gmail.com>
2022-01-19 09:01:22 +0900

    clean zsh files

---
+++ .zsh.d/function.zsh
@@ -0,0 +1,38 @@
+# checkout git branch(frb)
+fbr() {
+  local branches branch
+  branches=$(git --no-pager branch -vv) &&
+  branch=$(echo "$branches" | fzf +m) &&
+  git checkout $(echo "$branch" | awk '{print $1}' | sed "s/.* //")
+}
+
+# open pull request page(pr)
+pr () {
+    local branch url
+    branch=$(git rev-parse --abbrev-ref HEAD)
+    url=$(git config --local remote.origin.url)
+    open "$url/pull/$branch"
+}
+
+# full text search
+fzgrep() {
+  INITIAL_QUERY=""
+  RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+  FZF_DEFAULT_COMMAND="$RG_PREFIX '$INITIAL_QUERY'" \
+    fzf --bind "change:reload:$RG_PREFIX {q} || true" \
+        --ansi --phony --query "$INITIAL_QUERY" \
+        --preview 'cat `echo {} | cut -f 1 --delim ":"`'
+}
+
+# repository search(Ctrl+g)
+function peco-src () {
+  local selected_dir=$(ghq list -p | peco --query "$LBUFFER")
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
+  fi
+  zle clear-screen
+}
+zle -N peco-src
+bindkey '^g' peco-src
+
--- .zsh.d/pass.zsh
+++ .zsh.d/pass.zsh
@@ -1,2 +1,16 @@
+# homebrew
+if [ "$(uname -m)" = "arm64" ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  export PATH="/opt/homebrew/bin:$PATH"
+else
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
+
 # anyenv
 eval "$(anyenv init -)"
+
+# fzf option
+export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border --inline-info --preview 'head -100 {}'"
+export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "!.git/*"'
+export FZF_CTRL_T_OPTS='--preview "bat  --color=always --style=header,grid --line-range :100 {}"'
+
